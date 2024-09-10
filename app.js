11c77dbc1c4f69edc1c74fbfc6b705fc474c1254
Petersix <150849737+peterarcuri@users.noreply.github.com>
2024-09-10 03:34:29 -0400

    Initital commit

---
+++ app.js
@@ -0,0 +1,34 @@
+
+let favoriteNum = 11;
+let baseURL = "http://numbersapi.com";
+
+// ======================================
+// ======== PART 1: NUMBER FACTS ========
+// ======================================
+
+
+// // 1.)
+
+$.getJSON(`${baseURL}/${favoriteNum}?json`).then((data => {
+    console.log(data);
+}));
+
+// // 2.)
+
+let favoriteNums = [7, 18, 30];
+
+$.getJSON(`${baseURL}/${favoriteNums}?json`).then((data => {
+    console.log(data);
+}));
+
+// // 3.)
+
+Promise.all(
+    Array.from( { length: 4 }, () => {
+        return $.getJSON(`${baseURL}/${favoriteNum}?json`);
+    })
+).then(facts => {
+    facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+});
+
+
